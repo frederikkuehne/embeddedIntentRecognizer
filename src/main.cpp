33fcb715ae2ce0df095024085203c164f2fd4f27
Frederik Kühne <frk@tf.uni-kiel.de>
2022-02-12 14:08:54 +0100

    - implemented intent file parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <algorithm>
 #include <vector>
 using namespace std;
+
+#define MAIN_DELIMITER ";"
+#define SUB_DELIMITER ","
 
 #include "intent.hpp"
 
@@ -15,6 +20,8 @@ Intent* getIntent(vector<Intent*>* intents, string userInput);
  *-----------------------------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
+    vector<Intent*> intents;
+    Intent* guessedIntent;
 
     /* This application expects exactly one input: The user input string ------------------------*/
     if (argc != 2)
@@ -23,10 +30,12 @@ int main(int argc, char *argv[])
         cout << argv[0] << " <Input string to extract the intent from>" << endl;
         return 0;
     }
-
+    
+    /* Take first argument as user input and convert it to lower case ---------------------------*/
     string userInput = argv[1];
-    vector<Intent*> intents;
-    Intent* guessedIntent;
+    transform(userInput.begin(), userInput.end(), userInput.begin(),
+    [](unsigned char c){ return tolower(c); });
+
 
     /* Parses the list of intents to Intent objects from default path ---------------------------*/
     populateIntents(&intents);
@@ -46,7 +55,6 @@ void populateIntents(vector<Intent*>* intents)
     /* Create input file stream and open intent source line by line -----------------------------*/
     string line;
     ifstream intentSrc("./../data/intents");
-    string delimiterField = ";", delimiterSubField = ",";
     string intentName;
     vector<string> keywords, entities;
 
@@ -55,6 +63,8 @@ void populateIntents(vector<Intent*>* intents)
     {
         parseLine(line, &intentName, &keywords, &entities);
         intents->push_back(new Intent(intentName, keywords, entities));
+        keywords.clear();
+        entities.clear();
     }
 }
 
@@ -63,11 +73,45 @@ void populateIntents(vector<Intent*>* intents)
  *-----------------------------------------------------------------------------------------------*/
 void parseLine(string line, string* intentName, vector<string>* keywords, vector<string>* entities)
 {
-    *intentName = "Get weather";
-    keywords->push_back("weather");
-    keywords->push_back("rain");
-    entities->push_back("location");
-    entities->push_back("time");
+    size_t pos_start = 0, pos_end = 0, pos_sub_start = 0, pos_sub_end = 0;
+    string subLine;
+
+    /* Extract the intents name -----------------------------------------------------------------*/
+    pos_end = line.find(MAIN_DELIMITER, pos_start);
+    *intentName = line.substr(pos_start, pos_end);
+    cout << *intentName << endl;
+
+    /* Extract the intents keywords -------------------------------------------------------------*/
+    pos_start = pos_end + 1;
+    pos_end = line.find(MAIN_DELIMITER, pos_start);
+    subLine = line.substr(pos_start, pos_end - pos_start);
+
+    /* Iterate over all the keywords and push them into the keyword list ------------------------*/
+    while (pos_sub_end = subLine.find(SUB_DELIMITER, pos_sub_start))
+    {
+        keywords->push_back(subLine.substr(pos_sub_start, pos_sub_end - pos_sub_start));
+        pos_sub_start = pos_sub_end + 1;
+        if (pos_sub_end == string::npos)
+        {
+            break;
+        }
+    }
+
+    /* Extract the intents entities -------------------------------------------------------------*/
+    pos_start = pos_end + 1;
+    pos_end = line.find(MAIN_DELIMITER, pos_start);
+    subLine = line.substr(pos_start, pos_end - pos_start);
+
+    /* Iterate over all the entities and push them into the entity list -------------------------*/
+    while (pos_sub_end = subLine.find(SUB_DELIMITER, pos_sub_start))
+    {
+        entities->push_back(subLine.substr(pos_sub_start, pos_sub_end - pos_sub_start));
+        pos_sub_start = pos_sub_end + 1;
+        if (pos_sub_end == string::npos)
+        {
+            break;
+        }
+    }
 }
 
 /*-----------------------------------------------------------------------------------------------*
